ffb921343116a72d3b1dfbff1aab4a4cfebb0de9
Szabo Sara <szabosara.a1@gmail.com>
2025-02-18 10:51:42 +0200

    product model

--- src/Model/Product.java
+++ src/Model/Product.java
@@ -1,4 +1,46 @@
 package Model;
 
-public class Product {
+import Repository.Identifiable;
+
+/**
+ * Model representing a Medication.
+ */
+class Product implements Identifiable {
+    private int id;
+    private String name;
+    private double price;
+    private String universum;
+
+    public Product(String name, double price, String universum) {
+        this.name = name;
+        this.price = price;
+        this.universum = universum;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getUniversum() {
+        return universum;
+    }
+
+    @Override
+    public String toString() {
+        return "Medication{" + "id=" + id + ", name='" + name + '\'' + ", price=" + price + ", universum='" + universum + '\'' + '}';
+    }
 }
